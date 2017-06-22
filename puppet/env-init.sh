b4065d95b84b70ee5a7898cd988a27cf3b3706d4
vibeinitcron <vibe@initcron.org>
2017-06-22 15:16:41 +0530

    Delete env-init.sh

--- puppet/env-init.sh
+++
@@ -1,6 +0,0 @@
-export NAME=vijay
-
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update
-sudo apt-get install puppetserver
