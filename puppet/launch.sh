7b03dfbce8b12e195f1c3ddf8760c0ce285ed054
vibeinitcron <vibe@initcron.org>
2017-06-22 15:14:48 +0530

    Update launch.sh

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -2,5 +2,5 @@
 
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update
-sudo apt-get install puppetserver
+sudo apt-get update -y
+sudo apt-get install puppetserver -y
