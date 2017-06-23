9e415c0d9b2567600ecdac9c6304b02955bad223
vibeinitcron <vibe@initcron.org>
2017-06-23 16:49:44 +0530

    assets

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -6,8 +6,7 @@
 #sudo apt-get install puppetserver -y
 
 mkdir code
-ls
-pwd
-mv hello.pp manifest.pp code
-docker run -itd --name puppet --hostname puppet -v code:/etc/puppetlabs/code/ puppet/puppetserver
-docker exec -it puppet bash
+mv /home/scrapbook/tutorial/*.pp code
+ls code
+#docker run -itd --name puppet --hostname puppet -v code:/etc/puppetlabs/code/ puppet/puppetserver
+#docker exec -it puppet bash
