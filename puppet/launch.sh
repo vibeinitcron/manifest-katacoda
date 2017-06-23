5f9f7341987333ce9c9219d0a9222a18b1c55fb2
vibeinitcron <vibe@initcron.org>
2017-06-23 16:28:50 +0530

    assets

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update -y
-sudo apt-get install puppetserver -y
+#wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+#sudo dpkg -i puppetlabs-release-trusty.deb
+#sudo apt-get update -y
+#sudo apt-get install puppetserver -y
+
+mkdir code
+docker run -itd --name puppet --hostname puppet -v ./code:/etc/puppetlabs/code/ puppet/puppetserver
+docker exec -it puppet bash
